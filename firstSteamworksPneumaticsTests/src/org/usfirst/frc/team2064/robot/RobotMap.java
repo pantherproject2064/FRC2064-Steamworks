917d9f2c866b0c2f9ee9093cd2c5e78c929bb03f
pantherproject2064 <pantherproject2064@gmail.com>
2017-01-15 11:45:19 -0500

    More Progress

--- firstSteamworksPneumaticsTests/src/org/usfirst/frc/team2064/robot/RobotMap.java
+++ firstSteamworksPneumaticsTests/src/org/usfirst/frc/team2064/robot/RobotMap.java
@@ -17,12 +17,32 @@ public class RobotMap {
 	// public static int rangefinderPort = 1;
 	// public static int rangefinderModule = 1;
 	
-	//Pneumatics
-	public static final int COMPRESSOR = 0;
-	public static final int S_PIVOT_A = 0;
-	public static final int S_HOLD_B = 6;
-	public static final int S_HOLD_A = 1;
-	public static final int S_PIVOT_B = 7;
-	public static final Value = DoubleSolenoid
+	//Joysticks
+		public static int leftJoystick = 0;
+		public static int rightJoystick = 1;
+		
+	//Driving Motors
+		public static int driveRight1 = 0; //talon 0
+		public static int driveRight2 = 1; //talon 1
+		public static int driveLeft1 = 2; //talon 2
+		public static int driveLeft2 = 3; //talon 3
+		
+					// Pin for motor with encoder - 4 (1/15/17)
+					//encoder on pin 0,1
+	//Encoders
+		public static int 
 	
+	//Pneumatics
+		
+		//Pneumatics Wiring
+		public static final int COMPRESSOR = 0;
+		public static final int S_PIVOT_A = 0;
+		public static final int S_HOLD_B = 6;
+		public static final int S_HOLD_A = 1;
+		public static final int S_PIVOT_B = 7;
+		
+		//Pneumatics Constants
+		public static final Value = DoubleSolenoid.Value.kForward;
+		public static final Value = DoubleSolenoid.Value.kBackward;
+		
 }
